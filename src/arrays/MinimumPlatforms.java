b36f34f369380b91eaeb909d29c11e459cb00716
Karthik Sekar <karthiksekar@DESKTOP-F76QTA9>
2020-09-10 13:38:15 +0530

    Minimum Number of Platforms Required for a Railway/Bus Station

---
+++ src/arrays/MinimumPlatforms.java
@@ -0,0 +1,47 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MinimumPlatforms {
+	
+	static int findMaxPlatforms(int ari[],int dep[],int n)
+	{
+		Arrays.sort(ari);
+		Arrays.sort(dep);
+		
+		int platforms = 1, i=0, j=0, result=1;
+		
+		while(i<n&&j<n)
+		{
+			if(ari[i]<=dep[j])
+			{
+				platforms++;
+				i++;
+			}
+			
+			else if(ari[i]>dep[j])
+			{
+				platforms--;
+				j++;
+			}
+			
+			
+		}
+		
+		if(platforms>result)
+			result = platforms;
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+		int ari[] = {900, 940, 950, 1100, 1500, 1800};
+		int dep[] = {910, 1200, 1120, 1130, 1900, 2000};
+
+		int n = ari.length;
+		
+		System.out.println("Number of platforms required : "+findMaxPlatforms(ari,dep,n));
+	}
+
+}
